57b40c0b54245de77e76ecafbdb41eada23d8029
Alex Yin <yinchihkai0909@gmail.com>
2023-12-06 17:31:13 +0800

    update server.ts

---
+++ server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotEnv from "dotenv";
+dotEnv.config();
+
+const app = express();
+const port = process.env.SERVER_PORT || 8081;
+
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+// define cors allow domains
+const corsOptions = {
+  origin: "https://chihkaiyin.blog",
+};
+app.use(cors(corsOptions));
+
+app.listen(port, () => {
+  console.log(`server is listening to port ${port}`);
+});
