22bc938c7a3430512230bacc4ab850067080b467
Alex Yin <yinchihkai0909@gmail.com>
2023-12-07 11:40:53 +0800

    mysql connection

--- app/model/db.ts
+++ app/model/db.ts
@@ -1,7 +1,40 @@
-// db.config.ts saves the db info, eg: HOST, USER ...etc
-export const mySQLConfig = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_NAME,
-};
+import mysql from "mysql2";
+import { mySQLConfig } from "../config/db.config";
+
+export const connection = mysql.createConnection({
+  host: mySQLConfig.HOST,
+  user: mySQLConfig.USER,
+  password: mySQLConfig.PASSWORD,
+  database: mySQLConfig.DB,
+});
+
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Successfully connected to mysql");
+  }
+});
+
+// -- create table
+// CREATE TABLE post (
+// 	post_id INT AUTO_INCREMENT PRIMARY KEY,
+//     post_title VARCHAR(255) NOT NULL,
+//     post_content VARCHAR(1000) NOT NULL,
+//     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//     updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+// )
+// engine=innodb charset=utf8mb4 COLLATE utf8mb4_general_ci;
+
+// DESCRIBE post;
+
+// -- insert initial datas
+// INSERT INTO post(post_title, post_content) VALUES
+// ('title1', 'content1'),
+// ('title2', 'content2'),
+// ('title3', 'content3'),
+// ('title4', 'content4'),
+// ('title5', 'content5'),
+// ('title6', 'content6');
+
+// SELECT * FROM post;
--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import dotEnv from "dotenv";
 dotEnv.config();
 
